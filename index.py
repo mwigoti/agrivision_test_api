6d6af53f470249465d931e8d1ba1b00c047c65a0
mwigoti <henrymwoha02@gmail.com>
2024-11-07 09:09:14 +0300

    tiles

--- index.py
+++ index.py
@@ -3,9 +3,16 @@ import folium
 
 mapobj = folium.Map(location=[-1.2930392499491719, 36.817867215366086], zoom_start='stamenterrain')
 
-folium.TileLayer('stamenterrain', attr='stamenterrain').add_to(mapobj)
 
-folium.TileLayer('stamenterrain', attr='stamenterrain').add_to(mapobj)
+
+folium.TileLayer('stamenterrain').add_to(mapobj)
+
+	
+folium.TileLayer('mapquestopen').add_to(mapobj)
+	
+folium.TileLayer('MapQuest Open Aerial').add_to(mapobj)
+	
+folium.TileLayer('Mapbox Bright').add_to(mapobj)
 
 folium.LayerControl().add_to(mapobj)
 
